e8abb5df916591ac1e4f9fc9f0473abf60947621
ap6881198-bit <ap6881198@gmail.com>
2026-01-15 01:03:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+let currentPage = 0;
+const correctPassword = "pihu";
 
+function checkPassword() {
+  const input = document.getElementById("passInput").value.toLowerCase();
+  if (input === correctPassword) {
+    goToPage(1);
+  } else {
+    document.getElementById("wrongPass").innerText = "Wrong password 😝";
+  }
+}
+
+function nextPage() {
+  goToPage(currentPage + 1);
+}
+
+function goToPage(num) {
+  document.getElementById("page" + currentPage).classList.remove("active");
+  currentPage = num;
+  document.getElementById("page" + currentPage).classList.add("active");
+}
